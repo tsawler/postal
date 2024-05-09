c43c09c8aaa2c07d3ec683ecd9c739e73ef9ceb6
Trevor Sawler <trevor.sawler@gmail.com>
2024-05-09 11:42:10 -0300

    tests

---
+++ pool_test.go
@@ -0,0 +1,35 @@
+package postal
+
+import "testing"
+
+func Test_Run(t *testing.T) {
+	s := Service{
+		ServerURL:    "http://localhost",
+		SMTPServer:   "localhost",
+		SMTPPort:     1025,
+		SMTPUser:     "",
+		SMTPPassword: "",
+		ErrorChan:    make(chan error),
+		MaxWorkers:   2,
+		MaxMessages:  10,
+	}
+	dispatcher, _ := New(s)
+
+	dispatcher.Run()
+
+	msg := MailData{
+		ToName:      "Me",
+		ToAddress:   "me@here.com",
+		FromName:    "Jack",
+		FromAddress: "jack@there.com",
+		Subject:     "Test subject",
+		Content:     "Hello, world!",
+		CC:          []string{"you@here.com", "him@here.com"},
+	}
+
+	dispatcher.Send(msg)
+	err := <-service.ErrorChan
+	if err != nil {
+		t.Error("unexpected error when sending message")
+	}
+}
